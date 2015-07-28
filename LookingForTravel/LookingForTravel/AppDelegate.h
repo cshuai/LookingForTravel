72ce0229a87789090da2b5247fb161e059b83900
曹帅 <cshuai@shuai-MacBoook.local>
2015-07-28 11:10:21 +0800

    first commit

---
+++ LookingForTravel/LookingForTravel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LookingForTravel
+//
+//  Created by  曹帅 on 15/7/28.
+//  Copyright (c) 2015年 曹帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LookingForTravel/LookingForTravel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LookingForTravel
+//
+//  Created by  曹帅 on 15/7/28.
+//  Copyright (c) 2015年 曹帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
